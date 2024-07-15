a4999dd2fce5eff4a2ce434b4adb65ff5e4844ec
elinskjlm <elinskjlm@gmail.com>
2024-07-15 11:57:16 +0300

    Wrapped in catchAsync

--- routes/api.js
+++ routes/api.js
@@ -1,29 +1,30 @@
-const express = require('express');
+const express =     require('express');
 const { validateSchedule, validateForm } = require('../middleware');
-const formsAPI = require('../controllers/api/v1/forms')
-const schedsAPI = require('../controllers/api/v1/schedules')
-const router = express.Router();
+const catchAsync =  require('../utils/catchAsync')
+const formsAPI =    require('../controllers/api/v1/forms')
+const schedsAPI =   require('../controllers/api/v1/schedules')
+const router =      express.Router();
 
 router.route('/forms/:id')
-    .patch(formsAPI.toggleForm)
-    .delete(formsAPI.deleteForm);
+    .patch(catchAsync(formsAPI.toggleForm))
+    .delete(catchAsync(formsAPI.deleteForm));
 
 router.route('/forms')
-    .get(formsAPI.getAllForms)
-    .post(validateForm, formsAPI.createForm);
+    .get(catchAsync(formsAPI.getAllForms))
+    .post(catchAsync(validateForm, formsAPI.createForm));
 
 
-router.get('/schedules/getNames', schedsAPI.getNames)
-// router.get('/schedules/getWeeks', schedsAPI.getWeeks)
-// router.get('/schedules/getYears', schedsAPI.getYears)
+router.get('/schedules/getNames', catchAsync(schedsAPI.getNames));
+// router.get('/schedules/getWeeks', catchAsync(schedsAPI.getWeeks));
+// router.get('/schedules/getYears', catchAsync(schedsAPI.getYears));
 
 router.route('/schedules/:id')
-    .get(schedsAPI.getScheduleById)
-    .patch(schedsAPI.toggleSchedule)
-    .delete(schedsAPI.deleteSchedule);
+    .get(catchAsync(schedsAPI.getScheduleById))
+    .patch(catchAsync(schedsAPI.toggleSchedule))
+    .delete(catchAsync(schedsAPI.deleteSchedule));
 
 router.route('/schedules')
-    .get(schedsAPI.getAllScheds)
-    .post(validateSchedule, schedsAPI.createSchdule);
+    .get(catchAsync(schedsAPI.getAllScheds))
+    .post(catchAsync(validateSchedule, schedsAPI.createSchdule));
 
 module.exports = router;
--- routes/views.js
+++ routes/views.js
@@ -1,15 +1,16 @@
 const express =     require('express');
+const catchAsync =  require('../utils/catchAsync')
 const formsView =   require('../controllers/views/forms')
 const schedsView =  require('../controllers/views/schedules')
 const router =      express.Router();
 
-router.get(['/forms/apply', '/forms/apply/:id'], formsView.renderWorkerForm)
-// router.get('/forms/apply/:id',  formsView.renderWorkerForm)
+router.get(['/forms/apply', '/forms/apply/:id'], catchAsync(formsView.renderWorkerForm));
+// router.get('/forms/apply/:id',  catchAsync(formsView.renderWorkerForm));
 
-router.get('/forms/control', formsView.renderFormsControl)
+router.get('/forms/control', catchAsync(formsView.renderFormsControl));
 
-router.get('/schedules/control', schedsView.renderSchedsControl)
+router.get('/schedules/control', catchAsync(schedsView.renderSchedsControl));
 
-router.get('/schedules/read', schedsView.renderReadSched)
+router.get('/schedules/read', catchAsync(schedsView.renderReadSched));
 
 module.exports = router;
