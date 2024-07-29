45818a9dc963b1c002dcf22b6f8437eb2995468d
elinskjlm <elinskjlm@gmail.com>
2024-07-29 11:37:51 +0300

    Rearranged files and routes (is it restful now? 🤓)

--- routes/api.js
+++ routes/api.js
@@ -1,30 +1,39 @@
 const express =     require('express');
-const { validateSchedule, validateForm } = require('../middleware');
+const { validateSchedule, validateForm, /*isLoggedIn */} = require('../middleware');
 const catchAsync =  require('../utils/catchAsync')
 const formsAPI =    require('../controllers/api/v1/forms')
 const schedsAPI =   require('../controllers/api/v1/schedules')
+const usersAPI =    require('../controllers/api/v1/users')
 const router =      express.Router();
 
 router.route('/forms/:id')
-    .patch(catchAsync(formsAPI.toggleForm))
-    .delete(catchAsync(formsAPI.deleteForm));
+    .patch(/*isLoggedIn,*/ catchAsync(formsAPI.toggleForm))
+    .delete(/*isLoggedIn,*/ catchAsync(formsAPI.deleteForm));
 
 router.route('/forms')
-    .get(catchAsync(formsAPI.getAllForms))
-    .post(validateForm, formsAPI.createForm);
+    .get(/*isLoggedIn,*/ catchAsync(formsAPI.getAllForms))
+    .post(/*isLoggedIn,*/ validateForm, formsAPI.createForm); // How come no catchAsync TODO check
 
+router.route('/schedules')
+    .get(/*isLoggedIn,*/ /*catchAsync(*/schedsAPI.getAllScheds/*)*/)
+    .post(/*isLoggedIn,*/ /*catchAsync(*/validateSchedule, schedsAPI.createSchdule/*)*/);
 
-router.get('/schedules/getNames', catchAsync(schedsAPI.getNames));
+router.route('/schedules/:id')
+    .get(/*isLoggedIn,*/ catchAsync(schedsAPI.getScheduleById))
+    .patch(/*isLoggedIn,*/ catchAsync(schedsAPI.toggleSchedule))
+    .delete(/*isLoggedIn,*/ catchAsync(schedsAPI.deleteSchedule));
+
+router.get('/schedules/getNames', /*isLoggedIn,*/ catchAsync(schedsAPI.getNames));
 // router.get('/schedules/getWeeks', catchAsync(schedsAPI.getWeeks));
 // router.get('/schedules/getYears', catchAsync(schedsAPI.getYears));
 
-router.route('/schedules/:id')
-    .get(catchAsync(schedsAPI.getScheduleById))
-    .patch(catchAsync(schedsAPI.toggleSchedule))
-    .delete(catchAsync(schedsAPI.deleteSchedule));
+router.route('/users')
+    .get(/*isLoggedIn,*/ catchAsync(usersAPI.getAllUsers))
+    .post(/*isLoggedIn,*/ catchAsync(usersAPI.createUser));
 
-router.route('/schedules')
-    .get(/*catchAsync(*/schedsAPI.getAllScheds/*)*/)
-    .post(/*catchAsync(*/validateSchedule, schedsAPI.createSchdule/*)*/);
+router.route('/users/:id')
+    .get(/*isLoggedIn,*/ catchAsync(usersAPI.readUser))
+    .patch(/*isLoggedIn,*/ catchAsync(usersAPI.updateUser))
+    .delete(/*isLoggedIn,*/ catchAsync(usersAPI.deleteUser))
 
 module.exports = router;
