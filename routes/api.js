0c88c565a5bc91ba7076968091b97248475d7e63
elinskjlm <elinskjlm@gmail.com>
2024-08-02 16:33:55 +0300

    Fixed mistake (Order of routes)

--- routes/api.js
+++ routes/api.js
@@ -7,7 +7,7 @@ const usersAPI =    require('../controllers/api/v1/users')
 const router =      express.Router();
 
 router.route('/forms/:id')
-    .patch(isLoggedIn, catchAsync(formsAPI.toggleForm))
+    .patch(isLoggedIn,  catchAsync(formsAPI.toggleForm))
     .delete(isLoggedIn, catchAsync(formsAPI.deleteForm));
 
 router.route('/forms')
@@ -18,25 +18,26 @@ router.route('/schedules')
     .get(isLoggedIn, /*catchAsync(*/schedsAPI.getAllScheds/*)*/)
     .post(isLoggedIn, /*catchAsync(*/validateSchedule, schedsAPI.createSchdule/*)*/);
 
-router.route('/schedules/:id')
-    .get(isLoggedIn, catchAsync(schedsAPI.getScheduleById))
-    .patch(isLoggedIn, catchAsync(schedsAPI.toggleSchedule))
-    .delete(isLoggedIn, catchAsync(schedsAPI.deleteSchedule));
-
 router.get('/schedules/getNames', isLoggedIn, catchAsync(schedsAPI.getNames));
 // router.get('/schedules/getWeeks', catchAsync(schedsAPI.getWeeks));
 // router.get('/schedules/getYears', catchAsync(schedsAPI.getYears));
 
-router.route('/users')
-    .get(isLoggedIn, catchAsync(usersAPI.getAllUsers))
-    .post(isLoggedIn, catchAsync(usersAPI.createUser));
+router.route('/schedules/:id')
+    .get(isLoggedIn,    catchAsync(schedsAPI.getScheduleById))
+    .patch(isLoggedIn,  catchAsync(schedsAPI.toggleSchedule))
+    .delete(isLoggedIn, catchAsync(schedsAPI.deleteSchedule));
 
-router.post('/users/login', catchAsync(usersAPI.loginUser));
-router.post('/users/register', catchAsync(usersAPI.createUser));
+router.route('/users')
+    .get(isLoggedIn,    catchAsync(usersAPI.getAllUsers))
+    .post(isLoggedIn,   catchAsync(usersAPI.createUser));
+
+router.post('/users/login',     catchAsync(usersAPI.loginUser));
+router.get('/users/logout',     catchAsync(usersAPI.logoutUser));
+router.post('/users/register',  catchAsync(usersAPI.createUser));
 
 router.route('/users/:id')
-    .get(isLoggedIn, catchAsync(usersAPI.readUser))
-    .patch(isLoggedIn, catchAsync(usersAPI.updateUser))
+    .get(isLoggedIn,    catchAsync(usersAPI.readUser))
+    .patch(isLoggedIn,  catchAsync(usersAPI.updateUser))
     .delete(isLoggedIn, catchAsync(usersAPI.deleteUser))
 
 module.exports = router;
