7bc6b2357483660fb4e1e65c2b29e64241a6d9f3
elinskjlm <elinskjlm@gmail.com>
2024-08-13 13:37:03 +0300

    Minor changes

--- routes/views.js
+++ routes/views.js
@@ -7,19 +7,12 @@ const { isLoggedIn } = require('../middleware');
 const router = express.Router();
 
 router.get(['/forms/apply', '/forms/apply/:id'], formsView.renderWorkerForm);
-// router.get('/forms/apply/:id',  formsView.renderWorkerForm);
-
 router.get('/forms/control',     isLoggedIn, formsView.renderFormsControl);
 
 router.get('/schedules/control', isLoggedIn, schedsView.renderSchedsControl);
-
 router.get('/schedules/read',    isLoggedIn, schedsView.renderReadSched);
 
 router.get('/users/login',                   usersView.renderLogin);
 router.get('/users/control',     isLoggedIn, usersView.renderUsersControl);
-
-// router.post('/users/login', catchAsync(usersView.loginUser));
-// router.post('/users/register', catchAsync(usersView.registerUser));
-
 
 module.exports = router;
